3f60d27c9ac0f65f6fb9a2a813182be2e39a31b5
programador-negro <programador.negro@gmail.com>
2022-01-18 02:11:09 -0500

    primera carga

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+<section class="presentacion">
+  <div class="parallax">
+    <div class="nombre-cargo">
+      <img class="foto-perfil" src="../assets/img/daniel4.png" alt="">
+    <h2>
+        Full Stack Developer
+      </h2>
+      <p>
+        🍸 I design and code beautifully simple things, and I love what I do 🍹
+      </p>
+      <div class="hand-down">
+        👇
+      </div>
+    </div>
+  </div>
+    
+    <div class="about">
+      <h3>
+ Hi, I’m Daniel. Nice to meet you.
+ <br>👋
+      </h3>
+      <p>
+Since beginning my journey as a full stack developer nearly <b>2</b> years ago, I've done remote work for agencies, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+      </p>
+
+    </div>
+
+</section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  
+}
+</script>
+<style scoped>
+  @import url('../assets/css/home.css');
+  @import url('https://fonts.googleapis.com/css2?family=Neonderthaw&family=PT+Sans:ital,wght@0,700;1,700&display=swap');
+</style>
